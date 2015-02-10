307aa9207edfb80b4558daf72d19b4630053f769
Alexander Menk <a.menk@imi.de>
2015-02-10 16:45:04 +0100

    Ported Fix from n98-magerun in DB optimize

--- src/IMI/Contao/Command/Database/ImportCommand.php
+++ src/IMI/Contao/Command/Database/ImportCommand.php
@@ -47,23 +47,29 @@ HELP;
     protected function optimize($fileName)
     {
         $in = fopen($fileName,'r');
-        $result = tempnam(sys_get_temp_dir(),'dump') . '.sql';
+        $result = tempnam(sys_get_temp_dir(), 'dump') . '.sql';
         $out = fopen($result, 'w');
-
-        $current_table = '';
+        $currentTable = '';
         $maxlen = 8 * 1024 * 1024; // 8 MB
         $len = 0;
-        while($line = fgets($in)) {
+        while ($line = fgets($in)) {
             if (strtolower(substr($line, 0, 11)) == 'insert into') {
                 preg_match('/^insert into `(.*)` \(.*\) values (.*);/i', $line, $m);
+                if (count($m) < 3) { // fallback for very long lines or other cases where the preg_match fails
+                    if ($currentTable != '') {
+                        fwrite($out, ";\n");
+                    }
+                    fwrite($out, $line);
+                    $currentTable = '';
+                    continue;
+                }
                 $table = $m[1];
                 $values = $m[2];
-
-                if ($table != $current_table or ($len > $maxlen - 1000)) {
-                    if ($current_table != '') {
+                if ($table != $currentTable or ($len > $maxlen - 1000)) {
+                    if ($currentTable != '') {
                         fwrite($out, ";\n\n");
                     }
-                    $current_table = $table;
+                    $currentTable = $table;
                     $insert = 'INSERT INTO `' . $table . '` VALUES ' . $values;
                     fwrite($out, $insert);
                     $len = strlen($insert);
@@ -72,19 +78,18 @@ HELP;
                     $len += strlen($values) + 1;
                 }
             } else {
-                if ($current_table != '') {
+                if ($currentTable != '') {
                     fwrite($out, ";\n");
-                    $current_table = '';
+                    $currentTable = '';
                 }
                 fwrite($out, $line);
             }
-
         }
         fclose($in);
         fclose($out);
         return $result;
-
     }
+
     /**
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
