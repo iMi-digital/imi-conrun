166c383e76c69205076c179454843893a3b98454
Alexander Menk <a.menk@imi.de>
2015-08-14 10:43:16 +0200

    Fix

--- src/IMI/Contao/Command/Database/ImportCommand.php
+++ src/IMI/Contao/Command/Database/ImportCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use N98\Util\Exec;
+use IMI\Util\Exec;
 
 class ImportCommand extends AbstractDatabaseCommand
 {
