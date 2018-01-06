992120ee6b55254d1a1b35ef72f1d599abeff042
Alexander Menk <a.menk@imi.de>
2018-01-06 19:12:26 +0100

    New release 1.7.0

---
+++ RoboFile.php
@@ -0,0 +1,52 @@
+<?php
+use Symfony\Component\Finder\Finder;
+
+class RoboFile extends \Robo\Tasks
+{
+    /**
+     * Build the Robo phar executable.
+     */
+    public function pharBuild()
+    {
+        $this->_exec('phing');
+    }
+
+    public function iroboSign()
+    {
+        // signing key: a.menk@imi.de
+        $this->_exec('gpg -u 5AECD819 --detach-sign --output imi-conrun.phar.asc imi-conrun.phar');
+    }
+
+
+	/**
+	 * Publish iRobo to github
+	 *
+	 * @return \Robo\Result
+	 */
+    public function iroboPharPublish()
+    {
+    	$token = $this->ask('Github Token');
+    	$tag = $this->ask('Tag');
+    	$repo = 'imi-conrun';
+
+    	return $this->taskExecStack()
+		    ->exec('github-release release --security-token ' . escapeshellarg($token)
+		           . ' --user imi-digital --repo ' . $repo
+		           . ' --tag ' . escapeshellarg($tag))
+            // we upload a .phar and the same file without extension
+            // .phar seems to be needed for Phive installer
+		    ->exec('github-release upload --security-token ' . escapeshellarg($token)
+		           . ' --user imi-digital --repo ' . $repo
+		           . ' --tag ' . escapeshellarg($tag)
+		           . ' --file imi-conrun.phar'
+		           . ' --name imi-conrun.phar'
+		    )
+		    ->exec('github-release upload --security-token ' . escapeshellarg($token)
+		           . ' --user imi-digital  --repo ' . $repo
+		           . ' --tag ' . escapeshellarg($tag)
+		           . ' --file imi-conrun.phar.asc'
+		           . ' --name imi-conrun.phar.asc'
+		    )
+		    ->run();
+    }
+}
